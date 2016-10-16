a15ad78958073f86d50604a82981b2273d764835
ononoreo <daveonofrei@gmail.com>
2016-10-16 13:32:32 -0500

    Final Version w/o rbind() or data.frame

--- polluntantmean.R
+++ polluntantmean.R
@@ -1,7 +1,9 @@
 polluntantmean <- function(directory, polluntant, id = 1:332){
     csvfiles <- list.files(directory)[id]
+    meanvector <- c()
     for (datafile in csvfiles){
         filemean <- colMeans(read.csv(paste(directory, datafile, sep = "/"))[polluntant], na.rm = T)
-        print(filemean)
+        meanvector <- append(meanvector, filemean)
     }
+    print(mean(meanvector))
 }

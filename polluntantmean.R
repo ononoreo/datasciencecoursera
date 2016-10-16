63f1b30d20690f45dcf0157ea0a5b18a866a9b1b
ononoreo <daveonofrei@gmail.com>
2016-10-16 13:15:29 -0500

    version 2

--- polluntantmean.R
+++ polluntantmean.R
@@ -1,9 +1,7 @@
 polluntantmean <- function(directory, polluntant, id = 1:332){
     csvfiles <- list.files(directory)[id]
     for (datafile in csvfiles){
-        setwd(directory)
-        filemean <- colMeans(read.csv(datafile)[polluntant], na.rm = T)
+        filemean <- colMeans(read.csv(paste(directory, datafile, sep = "/"))[polluntant], na.rm = T)
         print(filemean)
-        setwd("../")
     }
 }
